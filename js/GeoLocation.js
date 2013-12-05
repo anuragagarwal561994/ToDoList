fef73b6c3447551fc0e1b987a36ecabf2114984b
anuragagarwal561994 <anurag.agarwal561994@gmail.com>
2013-12-06 02:40:29 +0530

    Correct the resizing of Map

--- js/GeoLocation.js
+++ js/GeoLocation.js
@@ -227,15 +227,18 @@ $('#main-nav-1').hide();
 $('#main-nav-2').show();
 $('#main-nav-3').hide();
 $(document).ready(function() {
-    initLocationProcedure();    
-    $('#map-canvas').css('height', $(window).height() - $('div.ui-navbar.ui-mini').height()-35-$('div.ui-input-search.ui-shadow-inset.ui-btn-corner-all.ui-btn-shadow.ui-icon-searchfield.ui-body-c').height());
+    initLocationProcedure();
 });
 
 $(window).resize(function(){
-    $('#map-canvas').css('height', $(window).height() - $('div.ui-navbar.ui-mini').height()-35-$('div.ui-input-search.ui-shadow-inset.ui-btn-corner-all.ui-btn-shadow.ui-icon-searchfield.ui-body-c').height());
+        $('#map-canvas').css('height', $(window).height() - $('div.ui-header.ui-bar-default').height()-35-$('div.ui-input-search.ui-shadow-inset.ui-btn-corner-all.ui-btn-shadow.ui-icon-searchfield.ui-body-c').height());
     $('#maincontent').css('height', $(window).height() - parseInt($('#main-header').css('height'))-3- parseInt($('#footer').css('height')));
     $('#set-location-content').css('height', $(window).height() - parseInt($("#set-location-header").css('height')) - parseInt($("#set-location-footer").css('height')))
 });
+$('#map').on('pageshow', function(event){
+    $('#map-canvas').css('height', $(window).height() - $('div.ui-header.ui-bar-default').height()-35-$('div.ui-input-search.ui-shadow-inset.ui-btn-corner-all.ui-btn-shadow.ui-icon-searchfield.ui-body-c').height());
+    google.maps.event.trigger(map, "resize");
+})
 $(document).on('pagechange', function(){
     $('#maincontent').css('height', $(window).height() - parseInt($('#main-header').css('height'))-3- parseInt($('#footer').css('height')));
     $('#set-location-content').css('height', $(window).height() - parseInt($("#set-location-header").css('height')) - parseInt($("#set-location-footer").css('height')));
